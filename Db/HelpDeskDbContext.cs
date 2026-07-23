da1ead13e792f8cfb6c555becc973628881622d8
rok1go <poznanskis.aleksandrs@gmail.com>
2026-07-23 13:38:22 +0300

    Set up database models and EF Core configuration

---
+++ Db/HelpDeskDbContext.cs
@@ -0,0 +1,15 @@
+﻿using HelpDesk_System.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HelpDesk_System.Db;
+
+public class HelpDeskDbContext : DbContext
+{
+    public HelpDeskDbContext(DbContextOptions<HelpDeskDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<User> Users => Set<User>();
+    public DbSet<Ticket> Tickets => Set<Ticket>();
+    public DbSet<TicketResponse> TicketResponses => Set<TicketResponse>();
+}
